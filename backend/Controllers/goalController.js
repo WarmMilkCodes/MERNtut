f40e224ba71158bce2f8f706de45977015464027
WarmMilkCodes <mrguybanks@gmail.com>
2022-02-21 23:37:25 -0600

    Commit

---
+++ backend/Controllers/goalController.js
@@ -0,0 +1,38 @@
+// @desc         Get goals
+// @route        GET /api/goals
+// @access       Private
+
+const getGoals = (req,res) => {
+    res.status(200).json({ message: "Get goals"})
+};
+
+// @desc         Set goals
+// @route        GET /api/goals
+// @access       Private
+
+const setGoals = (req,res) => {
+    res.status(200).json({ message: "Get goals"})
+};
+
+
+// @desc         Get goals
+// @route        GET /api/goals
+// @access       Private
+
+const getGoals = (req,res) => {
+    res.status(200).json({ message: "Get goals"})
+};
+
+
+// @desc         Get goals
+// @route        GET /api/goals
+// @access       Private
+
+const getGoals = (req,res) => {
+    res.status(200).json({ message: "Get goals"})
+};
+
+
+module.exports = {
+    getGoals,
+}
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const {getGoals} = require('../Controllers/goalController')
+
+router.get('/', (req,res) => {
+    res.status(200).json({ message: 'Get goals.'})
+});
+
+router.post('/', (req,res) => {
+    res.status(200).json({ message: 'Get goals.'})
+});
+
+router.put('/:id', (req,res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}`})
+});
+
+router.delete('/:id', (req,res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}`})
+});
+
+
+module.exports = router
